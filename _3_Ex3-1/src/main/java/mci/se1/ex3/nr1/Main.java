72f9eaa4b423952536cec49a85cdff821b48d39b
Dani-haid <danielamaria.stoehr@gmail.com>
2023-11-10 16:57:45 +0100

    generated class periodic table

--- _3_Ex3-1/src/main/java/mci/se1/ex3/nr1/Main.java
+++ _3_Ex3-1/src/main/java/mci/se1/ex3/nr1/Main.java
@@ -2,6 +2,12 @@ package mci.se1.ex3.nr1;
 
 public class Main {
     public static void main(String[] args) {
+        PeriodicTable myPeriodicTable = PeriodicTable.getInstance();
+        myPeriodicTable.addToTable(new ChemicalElement("Wasserstoff", "H", 1));
+        myPeriodicTable.addToTable(new ChemicalElement("Phosphor", "P", 15));
+
+        myPeriodicTable.printElements();
+
 
     }
 }
---
+++ _3_Ex3-1/src/main/java/mci/se1/ex3/nr1/PeriodicTable.java
@@ -0,0 +1,39 @@
+package mci.se1.ex3.nr1;
+
+public class PeriodicTable {
+    // periodical Table has a fixed number of entries (118) --> so i choose an array
+    // only problem is index 0
+    ChemicalElement[] elementList = new ChemicalElement[118];
+
+    // only 1 instance of Class should be created
+    private static PeriodicTable instance;
+
+    // private Constructor for Singleton Pattern
+    private PeriodicTable(){
+
+    };
+    public static PeriodicTable getInstance(){
+        if(instance == null){
+            instance = new PeriodicTable();
+        }
+        return instance;
+    }
+
+    public void addToTable(ChemicalElement element){
+        /**
+         * exception handling tbd
+         */
+        elementList[element.getAtomicNumber()] = element;
+    }
+
+    public void printElements(){
+        for (ChemicalElement element : elementList) {
+            if(element != null && element.getAtomicNumber()!= 0){
+                System.out.println("Printing Element| Ordnungszahl:" + element.getAtomicNumber()
+                        + " Symbol:" + element.getSymbol() + " Name:" + element.getName());
+            }
+        }
+    }
+
+
+}
