b257ed75a385a8de40956fd10698963c0b9e3c50
Dani-haid <danielamaria.stoehr@gmail.com>
2023-11-11 14:31:20 +0100

    created interface Drink

--- _3_Ex3-3/src/main/java/mci/se1/ex3/nr3/Main.java
+++ _3_Ex3-3/src/main/java/mci/se1/ex3/nr3/Main.java
@@ -2,6 +2,8 @@ package mci.se1.ex3.nr3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("initial commit");
+        // there's only 1 Machine, get Instance of it
+        AutomaticMachine automaticMachine = AutomaticMachine.getInstance();
+
     }
 }
---
+++ _3_Ex3-3/src/main/java/mci/se1/ex3/nr3/factory/Drink.java
@@ -0,0 +1,5 @@
+package mci.se1.ex3.nr3.factory;
+
+public interface Drink {
+    void prepare();
+}
