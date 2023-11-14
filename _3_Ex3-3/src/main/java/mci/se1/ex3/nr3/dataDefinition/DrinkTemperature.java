ac58f14c468c9759c1488eb263fd3f60c3fc623d
Dani-haid <danielamaria.stoehr@gmail.com>
2023-11-14 09:54:59 +0100

    added fixes

--- _3_Ex3-3/src/main/java/mci/se1/ex3/nr3/dataDefinition/DrinkTemperature.java
+++ _3_Ex3-3/src/main/java/mci/se1/ex3/nr3/dataDefinition/DrinkTemperature.java
@@ -7,21 +7,4 @@ public enum DrinkTemperature {
     HOT
 }
 
-/*
-public enum DrinkTemperature {
-    COLD("cold"),
-    WARM("warm"),
-    HOT("hot");
-    private String drinkTemperature;
-
-    DrinkTemperature(String inputTemperature){
-        this.drinkTemperature = inputTemperature;
-    }
-
-    public String getDrinkTemperature(){
-        return drinkTemperature;
-    }
-}
-*/
-
 
